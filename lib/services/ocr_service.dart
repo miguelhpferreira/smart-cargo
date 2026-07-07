82934ce3616f9085324e4ec90d41eb26b1b4cb22
miguelhpferreira <miguelhpferreira@gmail.com>
2026-07-06 21:08:03 -0300

    Criar lib/services/ocr_service.dart

---
+++ lib/services/ocr_service.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class OcrService {
+  final TextRecognizer _textRecognizer =
+      TextRecognizer(script: TextRecognitionScript.latin);
+
+  Future<String> readTextFromImage(File imageFile) async {
+    final inputImage = InputImage.fromFile(imageFile);
+    final recognizedText = await _textRecognizer.processImage(inputImage);
+    return recognizedText.text;
+  }
+
+  void dispose() {
+    _textRecognizer.close();
+  }
+}
